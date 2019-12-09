b900d82286bcf5ace9a67ab1afc49fec4e4d23ac
feixia5320 <1044489176@qq.com>
2019-12-09 21:07:32 +0800

    myangular

---
+++ myangular/src/app/product-detail/product-detail.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Product, ProductService, Comment} from '../shared/product.service';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+
+  product: Product;
+  comments: Comment[];
+  newRating = 5;
+  newComment = "";
+  isCommentHidden = true;
+
+  constructor(private routeInfo: ActivatedRoute,
+              private productService: ProductService
+              ) { }
+
+  ngOnInit() {
+    let productId: number = this.routeInfo.snapshot.params['productId'];
+    this.productService.getProduct(productId).subscribe(
+      product => this.product = product
+    );
+    this.productService.getCommentsForProductId(productId).subscribe(
+      comments => this.comments = comments
+    );
+  }
+  addComment(){
+    let comment = new Comment(0,this.product.id, new Date().toISOString(),"niuboabao",this.newRating,this.newComment);
+    this.comments.unshift(comment);
+    let sum = this.comments.reduce((sum, comment) => sum + comment.rating, 0);
+    this.product.rating = sum / this.comments.length;
+
+    this.newComment = null;
+    this.newRating = 5;
+    this.isCommentHidden = true;
+
+  }
+}
